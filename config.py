2da2e8f86f749ec2d007fcf281f16337194c6388
asnorkin <snorkin@phystech.edu>
2018-12-10 00:53:32 +0300

    Chapter XI added.
    Posts pagination was added for both explore and profile pages.

--- config.py
+++ config.py
@@ -15,3 +15,5 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['snorkin@phystech.com']
+
+    POSTS_PER_PAGE = 3
