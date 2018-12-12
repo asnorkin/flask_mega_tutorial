8083eb7048ab103fc670369a8a5fb33fdf75e35a
asnorkin <snorkin@phystech.edu>
2018-12-12 12:34:23 +0300

    travis added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel, lazy_gettext as _l
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -14,15 +15,22 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 migrate = Migrate(app, db)
 moment = Moment(app)
 
 from app import routes, models, errors
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if not app.debug:
